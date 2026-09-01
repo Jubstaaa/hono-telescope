e7f62ba70a9aa4a9e5c0e48e1fbaafb5d83c2bda
İlker Balcılar <ilkerbalcilartr@gmail.com>
2026-09-01 12:30:33 +0300

    feat: export the 1.0 public surface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+export { createTelescope } from './core/create-telescope';
+export type { Telescope } from './core/create-telescope';
+
+export { memoryStorage } from './core/storage/memory-storage';
+export type { MemoryStorageOptions } from './core/storage/memory-storage';
+export type { StorageAdapter, ListOptions } from './core/storage/storage-adapter';
+
+export { alsContext } from './core/context/als-context';
+export type { ContextStrategy, RequestContext } from './core/context/context-strategy';
+
+export { Recorder } from './core/recorder';
+
+export type { Collector } from './core/collectors/collector';
+export { consoleCollector } from './core/collectors/console-collector';
+export { exceptionCollector } from './core/collectors/exception-collector';
+export { fetchCollector } from './core/collectors/fetch-collector';
+
 export * from './types/index';
